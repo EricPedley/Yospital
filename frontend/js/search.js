7793cd636ac03a3da5907676b146ea2de09f67d3
Eric <ericpedley@gmail.com>
2019-09-21 23:47:52 -0700

    added another attribute to review summaries

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -18,7 +18,9 @@ $(document).ready(() => {
                     console.log("starting foreach");
                     loadData();
                     $('#zip').html('');
-                    $('#map').html('<div class="mapouter"><div class="gmap_canvas"><iframe width="590" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q='+zipcode+'&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/best-wordpress-themes/"></a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:590px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:590px;}</style></div>');
+                    
+                    $('#map').html('<center><div class="mapouter"><div class="gmap_canvas"><iframe width="590" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q='+zipcode+'&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/best-wordpress-themes/"></a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:590px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:590px;}</style></div></center>');
+                    //$('#map').html('<div class="mapouter"><div class="gmap_canvas"><iframe width="590" height="500" id="gmap_canvas" src="https://www.google.com/maps/search/hospitals/@37.7734358,-122.4400006,13.35z" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/best-wordpress-themes/"></a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:590px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:590px;}</style></div>');
                     $('#botheader').html('<font size=6>Hospitals Near You:</font>');
                     updateRatings();
                 });
@@ -47,10 +49,11 @@ function loadData() {
         let rating = hospital.rating;
         let cSens = rating["Cultural Sensitivity"];
         let hosp = rating["Hospitality"];
+        let qcare = rating["Quality of Care"];
         if(i===0)
             console.log(address.replace(/\s/g,"+"));
         let gmaps = "https://www.google.com/maps/place/"+address.replace(/\s/g,"+");
-        let element = '<div class="col-md-12 border summary"><center class="col-md-9"><font size="5"><a href="hospitalTemplate.html?id='+hospital.id+'">' + name + '</a></font></center><div class="col-md-3"><font size="4">Distance: ' + Math.round(proximity*10)/10 + ' Miles</font></div>'+'<div class="col-md-12"><center><a href ="'+gmaps+'">'+address+'</a></center></div>'+'<font size="4"><div class="col-md-4">Overall: <span class="rating" data-default-rating="' + (hosp + cSens) / 2 + '" disabled></span></div><div class="col-md-4">Hospitality: <span class="rating" data-default-rating="' + hosp + '" disabled></span></div><div class="col-md-4">Cultural Sensitivity: <span class="rating" data-default-rating="' + cSens + '" disabled></span></div></font></div>';
+        let element = '<div class="col-md-12 border summary"><center class="col-md-9"><font size="5"><a href="hospitalTemplate.html?id='+id+'">' + name + '</a></font></center><div class="col-md-3"><font size="4">Distance: ' + Math.round(proximity*10)/10 + ' Miles</font></div>'+'<div class="col-md-12"><center><a href ="'+gmaps+'">'+address+'</a></center></div>'+'<font size="4"><div class="col-md-3">Overall: <br><span class="rating" data-default-rating="' + (hosp + cSens) / 2 + '" disabled></span></div><div class="col-md-3">Hospitality: <br><span class="rating" data-default-rating="' + hosp + '" disabled></span></div><div class="col-md-3">Cultural Sensitivity: <br><span class="rating" data-default-rating="' + cSens + '" disabled></span></div><div class="col-md-3">Quality of Care: <br><span class="rating" data-default-rating="' + qcare + '" disabled></span></div></font></div>';
         $(element).appendTo("#summaries");
         counter++;
     }
