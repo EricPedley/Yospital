e587237565dc1fd519f58268b87503704b4baf8f
alexanduh <alexander.j.peng@gmail.com>
2019-09-22 04:09:47 -0700

    signout added

--- frontend/js/landing.js
+++ frontend/js/landing.js
@@ -11,11 +11,19 @@ firebase.initializeApp(firebaseConfig);
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-        document.getElementById("userDisplay").innerHTML = "Hello, " + user.email;
+        document.getElementById("userDisplay").innerHTML = "Hello, " + user.email + ". " + "<a onClick='signOut()'> Sign out?</a>";
     }
     else {
         document.getElementById("userDisplay").innerHTML = "Not logged in";
     }
 });
+
+function signOut() {
+    firebase.auth().signOut().then(function() {
+        console.log('Signed Out');
+      }, function(error) {
+        console.error('Sign Out Error', error);
+      });      
+}
 
 
